2f68c52489eba13739b0d669ec614019d7da01d2
Arnaud Viala <arnaud.viala@gmail.com>
2018-06-24 13:12:03 -0400

    Split all the rulers.js code into several methods

--- rulers.js
+++ rulers.js
@@ -1,128 +1,186 @@
+/*
+ *
+ * TODO:
+ *  - add a option in menu to hide rulers
+ *  - add the possibility to show only the vertical or horizontal ruler
+ */
+
 $(document).ready(function() {
 
-  var _OPTIONS = {
-    //show: "1",
-    unit: "px",
-    ref:  ".page"
-  }
+  // "rulers" singleton, embark all internal variables+methods necessary
+  var rulers = {
 
-  // example parameters :
-  //    ?ruler={unit:"px",ref:".page"}
-  //    ?ruler=unit:px;ref:.page
-  var urlParams = new URLSearchParams(window.location.search);
-  for( var o in _OPTIONS ) {
-    if ( urlParams.has('ruler-'+o) )
+    // default options
+    _OPTIONS: {
+      unit: "px",
+      ref:  ".page"
+    },
+
+    // contains the conversion ratio between "cm" or "in" to "pixels"
+    _RATIOS: { 'px':1 },
+
+    init: function() {
+      this._loadURLSearchParams();
+      this._computeRatios( ['cm','in'] );
+      this._addHTMLElements();
+      this._drawTicks();
+      this._moveTicks();
+
+      this._registerWindowEvents();
+    },
+
+    //  Internal: Look for "ruler-*" parameters in the URL's search
+    //            and populate the _OPTIONS members accordingly.
+    _loadURLSearchParams: function() {
+      var urlParams = new URLSearchParams(window.location.search);
+      for( var o in this._OPTIONS ) {
+        if ( urlParams.has('ruler-'+o) )
+        {
+          this._OPTIONS[o] = urlParams.get('ruler-'+o);
+        }
+      }
+    },
+
+    _computeRatios: function( units )
     {
-      _OPTIONS[o] = urlParams.get('ruler-'+o);
-    }
-  }
+      var div = $('<div>')
+        .css("visibility", "hidden")
+        .css("position", "absolute")
+        .css("top", "0px")
+        .css("left", "0px")
+        .css("margin","0")
+        .css("padding","0")
+        .css("border","none")
+        .appendTo( $(document.body) );
 
+      for( var u in units ) {
+        var unit = units[u];
+        div.css("width","1"+unit)
+           .css("height","1"+unit);
+        this._RATIOS[unit] = div.width();
+        if ( div.width() != div.height() )
+        {
+          console.error("Ouch, conversion '"+unit+"' to 'px' is different vertically/horizontally !");
+        }
+      }
 
-  // compute the relation between "1cm" and "pixels"
-  var _RATIOS = {};
-  (function(){
-    var div = $('<div>')
-      .css("visibility", "hidden")
-      .css("position", "absolute")
-      .css("margin","0")
-      .css("padding","0")
-      .css("border","none")
-      .css("width","1cm")
-      .css("height","1cm")
-      .appendTo( $(document.body) );  
-    _RATIOS['cm'] = div.width();
-    if ( div.width() != div.height() )
+      div.remove();
+    },
+
+    _HTMLElements: {},
+
+    _addHTMLElements: function()
     {
-      alert("Aïe, conversion 'cm' to 'px' is not squared !");
-    }
+      var divRulerH = $('<div>').addClass("h");
+      var divRulerV = $('<div>').addClass("v");
+      var divRulerMenu = $('<ul>').addClass("menu");//TODO:.hide();
+      var divRulerCorner = $('<div>').addClass("corner");
 
-    div.css("width","1in")
-      .css("height","1in");
-    _RATIOS['in'] = div.width();
-    if ( div.width() != div.height() )
+      var divRuler = $('<div>')
+        .addClass("ruler")
+        .append(divRulerV)
+        .append(divRulerH)
+        .append(divRulerMenu)
+        .append(divRulerCorner)
+        .appendTo($(document.body));
+
+      var liCursorPosition = $('<li>')
+        .append( $('<label>').text('Mouse:') )
+        .append( $('<label>').addClass("cursor-position") )
+        .appendTo(divRulerMenu);
+      var list = $('<select>')
+        .html( '<option value="px">px</option>'
+              +'<option value="cm">cm</option>'
+              +'<option value="in">in</option>' );
+      // TODO: make this dynamic based on an array [ "px", "cm", "in" ]
+
+      var liUnit = $('<li>')
+        .append( $('<label>').text('Unit:') )
+        .append( list )
+        .appendTo(divRulerMenu);
+      list.find("[value="+this._OPTIONS['unit']+"]")
+        .attr("selected", "1");
+
+      var self = this;
+      list.on('change',function(){
+        // TODO: write a subfunction self._onUnitChange( $(this).val() );
+        self._OPTIONS['unit'] = $(this).val();
+        // for now, we simply reload by changing URL's search
+        var urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('ruler-unit', self._OPTIONS['unit']);
+        window.location.search = urlParams.toString();
+      });
+
+      divRulerCorner
+        .data("ruler-menu",divRulerMenu)
+        .on('click',function(){
+          $(this).data("ruler-menu").toggle();
+        });
+
+      // everytime the document is resized, we would have to recompute ticks again
+      var divTicksH = $('<div class="ticks">')
+        .width($(document).width())
+        .css("height","100%")
+        .appendTo(divRulerH);
+      var divTicksV = $('<div class="ticks">')
+        .height($(document).height())
+        .css("width","100%")
+        .appendTo(divRulerV);
+
+      // Some special ticks that will follow the cursor position
+      var tCx = $('<s>')
+        .addClass("cursor")
+        .css("left","0px")
+        .append( $('<label>') )
+        .appendTo(divTicksH);
+      var tCy = $('<s>')
+        .addClass("cursor")
+        .css("top","0px")
+        .append( $('<label>') )
+        .appendTo(divTicksV);
+
+      // save HTML elements in an array
+      this._HTMLElements['divRuler'] = divRuler;
+      this._HTMLElements['divRulerCorner'] = divRulerCorner;
+      this._HTMLElements['divRulerMenu'] = divRulerMenu;
+      this._HTMLElements['divRulerH'] = divRulerH;
+      this._HTMLElements['divRulerV'] = divRulerV;
+      this._HTMLElements['divTicksH'] = divTicksH;
+      this._HTMLElements['divTicksV'] = divTicksV;
+      this._HTMLElements['divTickMouseH'] = tCx;
+      this._HTMLElements['divTickMouseV'] = tCy;
+    },
+
+    _registerWindowEvents: function()
     {
-      alert("Aïe, conversion 'in' to 'px' is not squared !");
-    }
-    
-    div.remove();
-  })();
+      var self = this;
+      $(window).scroll( function() {
+        self._moveTicks();
+      } );
+      $(window).resize(function() {
+        self._moveTicks();
+      });
 
-  var divRulerH = $('<div>').addClass("h");
-  var divRulerV = $('<div>').addClass("v");
-  var divRulerMenu = $('<ul>').addClass("menu");//TODO:.hide();
-  var divRulerCorner = $('<div>').addClass("corner");
+      $(window).mousemove( function(e) {
+        self._moveCursorTicks( { x: e.offsetX, y: e.offsetY } );
+      });
+    },
 
-  var divRuler = $('<div>')
-    .addClass("ruler")
-    .append(divRulerV)
-    .append(divRulerH)
-    .append(divRulerMenu)
-    .append(divRulerCorner)
-    .appendTo($(document.body));
+    //  Internal: repositionate the horizontal/vertical ".ticks" objects
+    //            so that relative x=0, y=0 match ref's {left,top}
+    _moveTicks: function()
+    {
+      var ref = $('.page');
+      // TODO: here, the reference '.page' is hard-coded
+      var offset = ref.offset();
+      offset.top -= $(document.body).scrollTop();
+      offset.left -= $(document.body).scrollLeft();
+      this._HTMLElements['divTicksH'].css("left",offset.left+"px");
+      this._HTMLElements['divTicksV'].css("top",offset.top+"px");
+    },
 
-  var liCursorPosition = $('<li>')
-    .append( $('<label>').text('Mouse:') )
-    .append( $('<label>').addClass("cursor-position") )
-    .appendTo(divRulerMenu);
-  var list = $('<select>')
-    .html( '<option value="px">px</option>'
-          +'<option value="cm">cm</option>'
-          +'<option value="in">in</option>' );
-
-  var liUnit = $('<li>')
-    .append( $('<label>').text('Unit:') )
-    .append( list )
-    .appendTo(divRulerMenu);
-  list.find("[value="+_OPTIONS['unit']+"]")
-    .attr("selected", "1");
-
-  list.on('change',function(){
-    _OPTIONS['unit'] = $(this).val();
-    // for now, we simply reload by changing URL's search
-    var urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('ruler-unit', _OPTIONS['unit']);
-    window.location.search = urlParams.toString();
-  });
-
-  divRulerCorner
-    .data("ruler-menu",divRulerMenu)
-    .on('click',function(){
-      $(this).data("ruler-menu").toggle();
-    });
-
-  // everytime the document is resized, we would have to recompute ticks again
-  var divTicksH = $('<div class="ticks">')
-    .width($(document).width())
-    .css("height","100%")
-    .appendTo(divRulerH);
-  var divTicksV = $('<div class="ticks">')
-    .height($(document).height())
-    .css("width","100%")
-    .appendTo(divRulerV);
-
-
-  var floor = function(x,b) { return Math.floor(x/b)*b; };
-
-  var offset = $('.page').offset();
-
-
-  $(window).scroll( function() {
-    // here, temporary, reupdate the left and top of 'ticks' bars
-    var offset = $('.page').offset();
-    offset.top -= $(document.body).scrollTop();
-    offset.left -= $(document.body).scrollLeft();
-    $('.ruler .h .ticks').css("left",offset.left+"px");
-    $('.ruler .v .ticks').css("top",offset.top+"px");
-  });
-
-  $(window).scroll(); // invoke the handler
-
-
-  var fnDrawTicks = function( unit )
-  {
-    var specs = {
+    _TICK_SPECS: {
       'px': {
-        ratio:      1,
         // here, we're using a decimal 'divider' =0.1 to express
         // we want to group 10px in each step
         divider:    0.1,
@@ -133,8 +191,6 @@ $(document).ready(function() {
         label:      10    // a label every 10 steps
       },
       'cm': {
-        // there are 37.7812 pixels in 1cm (on my computer !!)
-        ratio:      _RATIOS['cm'],
         divider:    2,    // each cm would be subdivided into 2 steps
         ticksSize:  [ 2, 6, 2, 6, 2, 6, 2, 6, 2, 10 ],
                     // size of ticks at each step
@@ -143,8 +199,6 @@ $(document).ready(function() {
         label:      10    // a label every 10 steps
       },
       'in': {
-        // there are 96 pixels in 1in (on my computer !!)
-        ratio:      _RATIOS['in'],
         divider:    16,   // each inch would be subdivided into 16 steps
         ticksSize:  [ 2, 4, 2, 6, 2, 4, 2, 6, 2, 4, 2, 6, 2, 4, 2, 10 ],
                     // size of ticks at each step
@@ -153,90 +207,93 @@ $(document).ready(function() {
                     // every 16 ticks (1in) -> 100%
         label:      16    // a label every 16 steps
       },
-    }
-    var getTickSize = function( spec, tick ) {
-      var index = (tick - 1) % (spec.ticksSize.length);
-      if ( index < 0 ) index += spec.ticksSize.length;
-      return ""+spec.ticksSize[index];
-    }
+    },
 
-
-    var spec = specs[unit];
-    var step = spec.ratio / spec.divider;
-    offset['floorTop'] = floor(offset.top, step);
-    offset['floorLeft'] = floor(offset.left, step);
-    for( var w2=-offset.floorLeft ; w2<($(document).width()-offset.floorLeft) ; w2+=step )
+    _drawTicks: function( unit, ref )
     {
-      var w = w2.toFixed(4);
-      var tick = Math.round( w / step );
+      // for now, param 'unit' are 'ref' are not set
+      // we have to initialize them here, which is weird
+      // (TODO: use local variables or real parameters)
+      if ( unit === undefined ) unit = this._OPTIONS['unit'];
+      if ( ref === undefined ) ref = $(this._OPTIONS['ref']);
 
-      var tClass = "t"+getTickSize(spec,tick);
-      var t = $('<s>').addClass(tClass)
-        .css("left",(w)+"px")
-        .appendTo(divTicksH);
-
-      if ( tick % spec.label == 0 ) {
-        var label = Math.round( w / (spec.divider*step) );
-        $('<label>').text(label).appendTo(t);
+      var getTickSize = function( spec, tick ) {
+        var index = (tick - 1) % (spec.ticksSize.length);
+        if ( index < 0 ) index += spec.ticksSize.length;
+        return ""+spec.ticksSize[index];
       }
-    }
-    for( var h2=-offset.floorTop ; h2<($(document).height()-offset.floorTop) ; h2+=step )
-    {
-      var h = h2.toFixed(4);
-      var tick = Math.round( h / step );
 
-      var tClass = "t"+getTickSize(spec,tick);
+      // round (floor) the number `x` to the integer count of `b`
+      //    the result might not be an integer (that's the case for 'cm')
+      //    it is `N * b` (where `N` is an integer)
+      var floor = function(x,b) { return Math.floor(x/b)*b; };
 
-      var t = $('<s>')
-        .addClass(tClass)
-        .css("top",(h)+"px")
-        .appendTo(divTicksV);
+      var spec = this._TICK_SPECS[unit];
+      var step = this._RATIOS[unit] / spec.divider;
+      var offset = ref.offset();
+      offset['floorTop'] = floor(offset.top, step);
+      offset['floorLeft'] = floor(offset.left, step);
+      for( var w2=-offset.floorLeft ; w2<($(document).width()-offset.floorLeft) ; w2+=step )
+      {
+        var w = w2.toFixed(4);
+        var tick = Math.round( w / step );
 
-      if ( tick % spec.label == 0 ) {
-        var label = Math.round( h / (spec.divider*step) );
-        $('<label>').text(label).appendTo(t);
+        var tClass = "t"+getTickSize(spec,tick);
+        var t = $('<s>').addClass(tClass)
+          .css("left",(w)+"px")
+          .appendTo( this._HTMLElements['divTicksH']);
+
+        if ( tick % spec.label == 0 ) {
+          var label = Math.round( w / (spec.divider*step) );
+          $('<label>').text(label).appendTo(t);
+        }
       }
-    }
-  };
+      for( var h2=-offset.floorTop ; h2<($(document).height()-offset.floorTop) ; h2+=step )
+      {
+        var h = h2.toFixed(4);
+        var tick = Math.round( h / step );
 
-  fnDrawTicks(_OPTIONS['unit']);
+        var tClass = "t"+getTickSize(spec,tick);
 
-  var tCx = $('<s>')
-  .addClass("cursor")
-  .css("left","0px")
-  .append( $('<label>') )
-  .appendTo(divTicksH);
-  var tCy = $('<s>')
-  .addClass("cursor")
-  .css("top","0px")
-  .append( $('<label>') )
-  .appendTo(divTicksV);
+        var t = $('<s>')
+          .addClass(tClass)
+          .css("top",(h)+"px")
+          .appendTo(this._HTMLElements['divTicksV']);
 
+        if ( tick % spec.label == 0 ) {
+          var label = Math.round( h / (spec.divider*step) );
+          $('<label>').text(label).appendTo(t);
+        }
+      }
+    },
 
-
-  $(window).mousemove( function(e) {
-    var left = e.offsetX - $('.ruler .h .ticks').position().left;
-    var top = e.offsetY - $('.ruler .v .ticks').position().top;
-    tCx.css("left", left+"px");
-    tCy.css("top", top+"px");
-
-    left = Math.floor( left );
-    top = Math.floor( top );
-
-    var unit = _OPTIONS['unit'];
-    if ( unit != "px" )
+    _moveCursorTicks: function( cursorPosition )
     {
-      var ratio = _RATIOS[unit];
-      left = (left / ratio).toFixed(2);
-      top = (top / ratio).toFixed(2);
-    }
-    
-    tCx.find('label').text(left);
-    tCy.find('label').text(top);
-    $('.ruler .cursor-position').text( left+" × "+top )
-  })
+      var left = cursorPosition.x - this._HTMLElements['divTicksH'].position().left;
+      var top = cursorPosition.y - this._HTMLElements['divTicksV'].position().top;
 
-  $(window).resize(function() {
-    $(window).scroll();
-  });
+      var tCx = this._HTMLElements['divTickMouseH'];
+      var tCy = this._HTMLElements['divTickMouseV'];
+      tCx.css("left", left+"px");
+      tCy.css("top", top+"px");
+
+      left = Math.floor( left );
+      top = Math.floor( top );
+
+      var unit = this._OPTIONS['unit'];
+      if ( unit != "px" )
+      {
+        var ratio = this._RATIOS[unit];
+        left = (left / ratio).toFixed(2);
+        top = (top / ratio).toFixed(2);
+      }
+
+      tCx.find('label').text(left);
+      tCy.find('label').text(top);
+      $('.ruler .cursor-position').text( left+" × "+top )
+    },
+
+  };  // end of singleton "rulers"
+
+  rulers.init();  // TODO: add options
 });
